58a9507b03f61d2ae606b90f56e2a6e7fc7eee56
Gaut9a8m <gaut9a8m@gmail.com>
2020-10-18 11:09:52 +0530

    new script in python

---
+++ browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+url1 = "https://mail.google.com/mail/u/0/#inbox"
+url2 = "https://hacktoberfest.digitalocean.com/profile"
+url3="https://internshala.com/"
+url4="https://www.olx.in/"
+url5="https://angel.co/jobs"
+webbrowser.open_new(url1)
+# webbrowser.open_new_tab(url1)
+webbrowser.open_new_tab(url2)
+webbrowser.open_new_tab(url3)
+webbrowser.open_new_tab(url4)
+webbrowser.open_new_tab(url5)
